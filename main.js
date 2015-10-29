8b02348381c45624222a3f476652cc36e7d341da
hhernandez008 <hhernandez008@gmail.com>
2015-10-29 16:12:04 -0700

    calculator v0.5 - Heather Hernandez

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 /* Global Variables */
 var my_calculator = new calculator(userInputs);//uses script from LearningFuze to pass params to userInputs function
-
 $(function(){
 
-    //Number Buttons & decimal button click handler
     $("button").on("click", function(){
+        //What is the value of the pressed button
         var val = $(this).text();
         switch (val){
+            // Clear everything in calculator memory (array)
             case 'AC':
                 my_calculator.allClear(val);
                 $numDisplay.text("");
                 break;
+            // Clear the current value in the calculator memory (array)
             case 'C':
                 my_calculator.clear(val);
                 $numDisplay.text("");
                 break;
+            //Add the pressed button's value to the calculator
             default:
                 my_calculator.addItem(val);
         }
-
     });
 
     var $numDisplay = $("#numberDisplay");
@@ -33,9 +34,8 @@ $(function(){
  * @param item
  */
 function userInputs(type, value, item){
-
+    //Print pressed button's value to the calculator
     $("#numberDisplay").text(value);
     console.log(type); //will be 'item added' 'calculated' or 'error'
-
 }
 
