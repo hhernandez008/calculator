1b3e9cef7e974cd2edd1f32e15033c70f5a3c6fa
hhernandez008 <hhernandez008@gmail.com>
2015-10-30 22:48:22 -0700

    update display to show equations & last value

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-var my_calculator = new calculator(userInputs);//uses script from LearningFuze to pass params to userInputs function
+var my_calculator = new calculator(displayCallback);//uses script from LearningFuze to pass params to userInputs function
+var equation = "";
 $(function(){
 
     $("button").on("click", function(){
@@ -10,11 +11,14 @@ $(function(){
             case 'AC':
                 my_calculator.allClear(val);
                 $numDisplay.text("");
+                $equDisplay.text("");
+                equation = "";
                 break;
             // Clear the current value in the calculator memory (array)
             case 'C':
                 my_calculator.clear(val);
                 $numDisplay.text("");
+                //TODO: how to clear last entered item from equation display.
                 break;
             //Add the pressed button's value to the calculator
             default:
@@ -23,17 +27,24 @@ $(function(){
     });
 
     var $numDisplay = $("#numberDisplay");
+    var $equDisplay = $("#equationDisplay");
 
 });//end document ready function
 
 
 /**
- * Take the user pressed button's value & add to the calculator display
+ * Take the user pressed button's value & add's it to the calculator display
  * @param type
  * @param value
  * @param item
  */
-function userInputs(type, value, item){
+function displayCallback(type, value, item){
+    if(type != "calculated") {
+        equation += value;
+        //display current equation
+        $("#equationDisplay").text(equation);
+        //TODO: display number string larger than one number in equation display correctly.
+    }
     //Print pressed button's value to the calculator
     $("#numberDisplay").text(value);
     console.log("Type: " + type); //will be 'item added' 'calculated' or 'error'
