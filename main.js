d326618d1fb7786e9ca813cab9363d7a12801e69
hhernandez008 <hhernandez008@gmail.com>
2015-10-31 20:07:38 -0700

    added clear functions, print to calculator display

--- main.js
+++ main.js
@@ -21,18 +21,18 @@ $(function(){
  * Take the user pressed button's value & add's it to the calculator display
  * @param type
  * @param value
- * @param item
+ * //@param item; to be added in later
  */
-function displayCallback(type, value, item){
+function displayCallback(type, value){
     if(type != "calculated") {
         equation += value;
         //display current equation
         $("#equationDisplay").text(equation);
-        //TODO: display number string larger than one number in equation display correctly.
+        //TODO: display number string larger than one number in equation display correctly. EX. 12 not 112
     }
     //Print pressed button's value to the calculator
     $("#numberDisplay").text(value);
-    console.log("Type: " + type); //will be 'item added' 'calculated' or 'error'
+    console.log("Type: " + type); //will be 'number', 'operator', or 'equalSign'
 }
 
 //check value function, checks if the button pressed is a operator, operand, decimal, AC, C, or +/-
@@ -94,22 +94,22 @@ function calcOperation(value){
                 break;
             case "add":
                 lastPressed = "";
-                valObject.value = "add";
+                valObject.value = "+";
                 equationArray.push(valObject);
                 break;
             case "subtract":
                 lastPressed = "";
-                valObject.value = "subtract";
+                valObject.value = "-";
                 equationArray.push(valObject);
                 break;
             case "multiply":
                 lastPressed = "";
-                valObject.value = "multiply";
+                valObject.value = "*";
                 equationArray.push(valObject);
                 break;
             case "divide":
                 lastPressed = "";
-                valObject.value = "divide";
+                valObject.value = "/";
                 equationArray.push(valObject);
                 break;
             default: //equals
@@ -131,33 +131,32 @@ function calcOperation(value){
             equationArray.push(valObject);
         }
     } //end if/else
+
     //FOR TESTING: what is the value of lastPressed?
     console.log(lastPressed);
     //FOR TESTING: what is in the array?
     console.log(equationArray);
-
+    displayCallback(valObject.type, valObject.value);
 } //end calcOperation function
 
 //solve equation function, checks the operand pressed and solves the equation of the first two values stored( 1 +
 // 3), does not take into account order of operations as numbers are equated as entered, Returns solved equation
 
 
-//clearAll function, clear all of the values from array & clear display
 
+// clear all of the values from array & clear display
+function clearAll(){
+    $("#numberDisplay").text("");
+    $("#equationDisplay").text("");
+    equation = "";
+    equationArray = [];
+    lastPressed = "";
+}
 
-//clear function, clear last inputted number group or operator from array & display & equation display
-
-/*
-// Clear everything in calculator memory (array)
-case 'AC':
-clearAll();
-$numDisplay.text("");
-$equDisplay.text("");
-equation = "";
-break;
-// Clear the current value in the calculator memory (array)
-case 'C':
-clear(val);
-$numDisplay.text("");
-//TODO: how to clear last entered item from equation display.
-break;*/
+// clear last inputted button group or operator from array, number display, & equation display
+function clear(){
+    $("#numberDisplay").text("");
+    //TODO: how to clear last entered item from equation display.
+    equationArray.pop();
+    lastPressed = "";
+}
